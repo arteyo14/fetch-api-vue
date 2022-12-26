637d789e5e790a9fc0fe9cd8647f36c98b767c9c
arteyo14 <98311957+arteyo14@users.noreply.github.com>
2022-12-26 16:01:23 +0700

    [develop] -m fetching Data and creating web template and add some style

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,41 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, reactive, toRefs } from 'vue';
+import AsideComp from './components/AsideComp.vue'
+import UserDetail from './components/UserDetail.vue';
+
+// const URL = "https://randomuser.me/api/?results=24";
+
+// const data = reactive({
+//     items: [],
+//     loading: false,
+//     error: null
+// })
+
+// const getData = async () => {
+//     try {
+//         data.loading = true;
+//         const response = await fetch(URL);
+//         data.items = await response.json();
+//     } catch (error) {
+//         data.error = error;
+//     } finally {
+//         data.loading = false;
+//     }
+// }
+
+// onMounted(getData)
+
+// newData = {
+//     ...toRefs(data),
+// }
+
+// console.log(data.items)
+
 </script>
 
 <template>
-  
+  <div class="grid p-20 items-center h-[100vh] grid-cols-[minmax(350px,_33%)_1fr] max-width-[1200px]">
+    <AsideComp></AsideComp>
+    <UserDetail></UserDetail>
+  </div>
 </template>
---
+++ src/components/AsideComp.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, onMounted, computed } from 'vue';
+import getData from '../service/service';
+
+const users = ref(null)
+const searchTerms = ref("");
+
+onMounted(() => getData(users));
+
+</script>
+
+<template>
+    <section class="w-[100%] h-[100%] bg-white overflow-auto">
+        <div class="sticky top-0">
+            <div class="flex items-center p-4 bg-[#EDEDED] h-20">
+                <img class="mr-4 w-16 h-16 rounded-full" src="https://randomuser.me/api/portraits/men/1.jpg" alt="user-img">
+                <span>
+                    <h1 class="text-[#999999] text-lg font-extrabold">YOUR NAME</h1>
+                </span>        
+            </div>
+            <div class="flex items-center p-4 bg-[#F6F6F6] h-14">
+                <div class="flex w-[100%] items-center gap-4 bg-white rounded-full p-2 ">
+                    <label class="ml-2" for="search__input">search</label>
+                    <input v-model="searchTerms" class="w-full h-[100%] focus:outline-none rounded-lg bg-transparent" id="search__input" type="search" placeholder="Search your friend">
+                </div>
+            </div>
+        </div>
+        <div class="overflow-auto">
+            <ul class="w-full" v-for="user in users" :key="user.id">
+                <li class="flex w-[100%] items-center gap-4 px-4 py-2 hover:bg-[#ededed] cursor-pointer">
+                    <img class="w-18 h-18 rounded-full" :src="user.picture.medium" alt="user-img">
+                    <div>
+                        <p class="text-xl font-bold">{{ user.name.title }}.{{ user.name.first }}  {{ user.name.last }}</p>
+                        <span class="text-[#999]">{{ user.email }}</span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
+    
+</template>
